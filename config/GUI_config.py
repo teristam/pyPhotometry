684940a7d6e232c9e801ce4459a61c6c5ef2b857
Teris <teristam@gmail.com>
2023-09-06 14:29:01 +0100

    change default file type

--- config/GUI_config.py
+++ config/GUI_config.py
@@ -8,10 +8,10 @@ history_dur = 10  # Duration of plotted signal history (seconds)
 triggered_dur = [-3, 6.9]  # Window duration for event triggered signals (seconds pre, post)
 update_interval = 5  # Interval between calls to update function (ms).
 
-default_LED_current = [10, 10]  # Channel [1, 2] (mA).
+default_LED_current = [30, 30]  # Channel [1, 2] (mA).
 
 default_acquisition_mode = (
     "1 colour continuous + 2 colour time div."  # Valid values: '2 colour continuous', '1 colour time div.', '2 colour time div.'
 )
 
-default_filetype = "csv"  # Valid values: 'ppd', 'csv'
+default_filetype = "ppd"  # Valid values: 'ppd', 'csv'
